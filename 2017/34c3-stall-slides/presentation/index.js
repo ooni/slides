83618895390c8111334858cb7821ab627943124c
Arturo Filastò <arturo@filasto.net>
2017-12-24 18:23:05 +0100

    Duplicate slides for stand slides

---
+++ 2017/34c3-stall-slides/presentation/index.js
@@ -0,0 +1,444 @@
+// Import React
+import React from "react"
+
+import styled from 'styled-components'
+
+import chroma from 'chroma-js'
+import { colors } from 'ooni-components'
+
+import FaApple from 'react-icons/lib/fa/apple'
+import FaAndroid from 'react-icons/lib/fa/android'
+import MdLaptop from 'react-icons/lib/md/laptop'
+import MdChatBubble from 'react-icons/lib/md/chat-bubble'
+
+import QRCode from 'qrcode.react'
+
+// Import Spectacle Core tags
+import {
+  Image,
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text,
+  Layout,
+  Fit,
+  Fill,
+  Appear,
+  Link
+} from "spectacle"
+
+
+// Import theme
+import theme from "../themes/ooni/index.js";
+
+import WorldDots from './WorldDots'
+
+const images = {
+  CCCLogo: require("../assets/34C3Logo.svg"),
+  OONIHorizontalColor: require("ooni-components/components/svgs/logos/OONI-HorizontalColor.svg"),
+  OONIVerticalColor: require("ooni-components/components/svgs/logos/OONI-VerticalColor.svg"),
+  ProbeVerticalColor: require("ooni-components/components/svgs/logos/Probe-VerticalColor.svg"),
+  ProbeHorizontalColor: require("ooni-components/components/svgs/logos/Probe-HorizontalColor.svg"),
+  PipelineHorizontalMonochrome: require("ooni-components/components/svgs/logos/Pipeline-HorizontalMonochrome.svg"),
+  APIHorizontalMonochrome: require("ooni-components/components/svgs/logos/API-HorizontalMonochrome.svg"),
+  ExplorerHorizontalMonochrome: require("ooni-components/components/svgs/logos/Explorer-HorizontalMonochrome.svg"),
+  FlagOfCuba: require("../assets/Flag_of_Cuba.svg"),
+  FlagOfEthiopia: require("../assets/Flag_of_Ethiopia.svg"),
+  FlagOfIndonesia: require("../assets/Flag_of_Indonesia.svg"),
+  FlagOfIran: require("../assets/Flag_of_Iran.svg"),
+  FlagOfPakistan: require("../assets/Flag_of_Pakistan.svg"),
+  BlockedDomainsInIran: require("../assets/BlockedDomainsInIran.svg"),
+  MiddleBoxes: require("../assets/MiddleBoxes.svg"),
+  PerformanceBolt: require("../assets/PerformanceBolt.svg"),
+  WebCensorshipCross: require("../assets/WebCensorshipCross.svg"),
+  OnionBlue: require("../assets/Onion_Blue_Icon.svg"),
+  Nexus5: require("../assets/Nexus5.svg"),
+  Nexus5Left: require("../assets/Nexus5Left.svg"),
+  Nexus5Right: require("../assets/Nexus5Right.svg"),
+  MobileAppScreenshot1: require("../assets/MobileAppScreenshot1.jpg"),
+  MobileAppScreenshot2: require("../assets/MobileAppScreenshot2.jpg"),
+  MobileAppScreenshot3: require("../assets/MobileAppScreenshot3.jpg"),
+  MobileAppScreenshot4: require("../assets/MobileAppScreenshot4.jpg"),
+  MobileAppScreenshot5: require("../assets/MobileAppScreenshot5.jpg"),
+  OONIAPIScreenshot: require("../assets/OONIAPI-Screenshot.png"),
+  OONIExplorerScreenshot: require("../assets/OONIExplorer-Screenshot.png"),
+  OONIProbeScreenshot: require("../assets/OONIProbe-Screenshot.png"),
+  OONIRunScreenshot: require("../assets/OONIRun-Screenshot.png"),
+};
+
+const GRID_HEIGHT = 400
+const GRID_WIDTH = 300
+const ECOSYSTEM_IMG_W = null
+const ECOSYSTEM_IMG_H = 200
+
+const Arrow = ({dir}) => {
+  let transform
+  let width
+  let height
+  let viewBox = '0 0 140 140'
+  if (dir == 'down') {
+    transform = 'rotate(90) translate(0 -40)'
+    viewBox = '0 0 40 140'
+    height = '200'
+  } else if (dir == 'up') {
+    transform = 'rotate(-90) translate(-140 0)'
+    viewBox = '0 0 40 140'
+    height = '200'
+  } else if (dir == 'left') {
+    transform = 'rotate(180) translate(-140 -40)'
+    viewBox = '0 0 140 40'
+    width = '200'
+  } else {
+    width = '200'
+  }
+  return (<svg viewBox={viewBox} width={width} height={height}>
+  	<g transform={transform}><path d="M120,6.6L133.4,20L120,33.4l-2.3-2.4l9.3-9.4H6.6v-3.2H127L117.7,9L120,6.6z"/></g>
+  </svg>
+  )
+}
+const ooEcosystem = {
+  ProbeVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Probe-VerticalMonochrome.svg"),
+    top: 0,
+    left: 0
+  },
+  SnycVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Snyc-VerticalMonochrome.svg"),
+    top: GRID_HEIGHT,
+    left: 0
+  },
+
+  RunVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Run-VerticalMonochrome.svg"),
+    top: 0,
+    left: GRID_WIDTH
+  },
+
+  ExplorerVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Explorer-VerticalMonochrome.svg"),
+    top: 0,
+    left: GRID_WIDTH*2,
+  },
+  APIVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/API-VerticalMonochrome.svg"),
+    top: GRID_HEIGHT,
+    left: GRID_WIDTH*2,
+  },
+
+  ProteusVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Proteus-VerticalMonochrome.svg"),
+    top: GRID_HEIGHT/2,
+    left: GRID_WIDTH*3,
+  },
+
+  PipelineVerticalMonochrome: {
+    src: require("ooni-components/components/svgs/logos/Pipeline-VerticalMonochrome.svg"),
+    top: GRID_HEIGHT,
+    left: GRID_WIDTH*3
+  },
+}
+
+// Require CSS
+require("normalize.css");
+
+const WatermarkImage = styled(Image)`
+position: absolute;
+top: 0;
+right: 0;
+`
+
+const DefinitionTerm = styled(Text)`
+font-size: 3.5rem !important;
+text-align: left;
+`
+
+const DefinitionFonetics = styled(Text)`
+  font-size: 2.5rem !important;
+  color: ${colors.palette.gray4};
+  display: inline;
+`
+
+const DefinitionItem = styled(ListItem)`
+  text-align: left;
+  padding-bottom: 20px;
+  font-size: 2rem !important;
+  text-indent: -30px;
+  padding-left: 30px;
+  color: ${colors.palette.gray2};
+`
+
+const Definition = styled.span`
+  color: ${colors.palette.black};
+`
+
+const WorldDotsBg = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: '50%';
+  transform: translateX(-50%) translateY(-20%);
+
+  z-index: -1;
+  opacity: 0.8;
+  filter: blur(3px)
+`
+
+const CountryReportHeading = styled.div`
+  background-color: ${colors.OONI_BLUE};
+  color: ${colors.palette.white};
+  font-size: 3rem;
+  padding: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`
+
+const CountryReportFinding = styled.div`
+  background-color: ${chroma(colors.palette.gray9).alpha(0.7).css()};
+  color: ${colors.palette.white};
+  padding: 20px;
+  margin-top: 20px;
+  text-align: left;
+`
+const WhiteImageContainer = styled.div`
+  background-color: ${colors.palette.white};
+`
+
+const EcosystemContainer = styled.div`
+  position: relative;
+  height: 700px;
+  width: 1000px;
+`
+
+const EcosystemItem = styled.div`
+  position: absolute;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
+`
+
+const ArrowContainer = styled.div`
+  position: absolute;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
+`
+
+const StyledFloatingLink = styled.div`
+bottom: calc(-120vh - 50%);
+left: calc(-50vw + 50%);
+position: absolute;
+width: 100vw;
+height: 100vh;
+`
+
+const StyledPageLinkContainer = styled.div`
+  text-align: left;
+  padding-left: 100px;
+  padding-top: 50px;
+`
+
+const StyledPageLink = styled(Link)`
+  position: relative;
+  padding-left: 10px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 30px;
+  background-color: ${colors.palette.white};
+  border: 2px solid ${colors.palette.gray6};
+  border-radius: 10px;
+  box-shadow: 5px 10px 10px #888888;
+`
+
+const FloatingLink = ({url}) => (
+  <StyledFloatingLink>
+    <StyledPageLinkContainer>
+      <StyledPageLink href={url}>{url}</StyledPageLink>
+    </StyledPageLinkContainer>
+  </StyledFloatingLink>
+)
+
+const NettestType = styled(Fill)`
+  padding-bottom: 20px;
+  h4 {
+    font-size: 2rem;
+    color: ${colors.palette.black}
+  }
+  svg {
+    fill: ${colors.palette.blue5}
+  }
+`
+
+const PhoneContainer = styled(Fill)`
+  position: relative;
+  perspective: 500px;
+  opacity: ${props => props.active ? '1' : '0.7'};
+  transition: opacity 3s;
+`
+
+const Screenshot = styled.div`
+  position: absolute;
+  top: 70px;
+  left: 15px;
+  img {
+    width: 275px;
+    margin: 0;
+  }
+`
+
+const PhoneAndScreenshot = styled.div`
+  ${props => {
+    if (props.active) {
+      return '';
+    }
+    return 'transform: translateZ(-200px);';
+  }};
+  transition: transform 3s;
+`
+
+class PhoneCarousel extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      activeIdx: 0
+    }
+    this.change = this.change.bind(this)
+  }
+
+  componentDidMount(){
+    this.change()
+    setInterval(this.change, 15000)
+  }
+
+  change() {
+    this.setState({
+      activeIdx: (this.state.activeIdx + 1) % 3
+    })
+  }
+  render() {
+    const {
+      activeIdx
+    } = this.state
+    const {
+      screenshots
+    } = this.props
+
+    return (
+      <Layout>
+        {[0,1,2].map(idx => {
+          return (
+            <PhoneContainer active={idx == activeIdx}>
+              <PhoneAndScreenshot active={idx == activeIdx}>
+                <Screenshot>
+                  <Image src={screenshots[idx]} />
+                </Screenshot>
+                <Image src={images.Nexus5} width="300px" />
+              </PhoneAndScreenshot>
+            </PhoneContainer>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+const StyledQRCodeContainer = styled.div`
+  bottom: calc(-50vh + 45%);
+  left: calc(-50vw - 10%);
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+`
+
+const StyledQRCodeValue = styled.div`
+  color: ${colors.palette.blue5};
+  font-size: 15px;
+`
+
+const StyledQRCode = ({value, hideText}) => {
+  return (
+    <StyledQRCodeContainer>
+      <QRCode value={value} />
+      {!hideText && <StyledQRCodeValue>{value}</StyledQRCodeValue>}
+    </StyledQRCodeContainer>
+  )
+}
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+        <Slide transition={["zoom"]}>
+          <StyledQRCode hideText value="https://ooni.torproject.org/" />
+          <Image width="300px" src={images.OONIVerticalColor} align='right'/>
+          <Heading size={3} textColor="primary">The Open Observatory of Network Interference</Heading>
+          <WorldDotsBg>
+            <WorldDots primaryColor={colors.palette.blue5} highlightColor={colors.palette.blue9}/>
+          </WorldDotsBg>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary">
+          <PhoneCarousel screenshots={[
+            images.MobileAppScreenshot1,
+            images.MobileAppScreenshot2,
+            images.MobileAppScreenshot3
+          ]}/>
+          <StyledQRCode hideText value="https://ooni.torproject.org/install" />
+          <Image height="100px" src={images.ProbeHorizontalColor} />
+        </Slide>
+
+        <Slide transition={["fade"]}>
+          <Heading size={2} textColor="primary">OONI Software tests</Heading>
+          <Layout style={{paddingTop: '40px'}}>
+            <NettestType>
+              <Image src={images.WebCensorshipCross} height="150px" />
+              <Heading size={4}>Web Censorship</Heading>
+            </NettestType>
+
+            <NettestType>
+              <MdChatBubble size="150px" />
+              <Heading size={4}>Instant Messaging Apps</Heading>
+            </NettestType>
+
+            <NettestType>
+              <Image src={images.OnionBlue} height="145px" />
+              <Heading size={4}>Censorship Circumvention</Heading>
+            </NettestType>
+
+          </Layout>
+          <Layout style={{paddingTop: '20px'}}>
+            <NettestType>
+              <Image src={images.MiddleBoxes} height="150px" />
+              <Heading size={4} style={{paddingTop: '20px'}}>Middle Boxes</Heading>
+            </NettestType>
+
+            <NettestType>
+              <Image src={images.PerformanceBolt} height="150px" />
+              <Heading size={4} style={{paddingTop: '20px'}}>Speed & Performance</Heading>
+            </NettestType>
+          </Layout>
+        </Slide>
+
+
+        <Slide transition={["fade"]} maxHeight="100vh" maxWidth="100vw" bgImage={images.OONIProbeScreenshot}>
+        </Slide>
+
+        <Slide transition={["fade"]} maxHeight="100vh" maxWidth="100vw" bgImage={images.OONIRunScreenshot}>
+          <FloatingLink url='https://run.ooni.io' />
+        </Slide>
+
+        <Slide transition={["fade"]} maxHeight="100vh" maxWidth="100vw" bgImage={images.OONIExplorerScreenshot}>
+          <FloatingLink url='https://explorer.ooni.io' />
+        </Slide>
+
+        <Slide transition={["fade"]} maxHeight="100vh" maxWidth="100vw" bgImage={images.OONIAPIScreenshot}>
+          <FloatingLink url='https://api.ooni.io' />
+        </Slide>
+
+      </Deck>
+    );
+  }
+}
